13a03260c374039fa7179f7b8309c4428075ae27
haha58 <9312877+haha58@user.noreply.gitee.com>
2026-06-23 14:01:35 +0800

    增加阻止代码

---
+++ packages/contracts/src/index.ts
@@ -0,0 +1,30 @@
+export type {
+  ApiError,
+  ApiFailure,
+  ApiMeta,
+  ApiResponse,
+  ApiSuccess,
+} from './common/response'
+
+export {
+  buildFailure,
+  buildSuccess,
+} from './common/response'
+
+export {
+  BizCode,
+} from './common/biz-code'
+
+export type {
+  BizCode as BizCodeValue,
+} from './common/biz-code'
+
+export {
+  PingRequestSchema,
+  PingResponseSchema,
+} from './system/ping.contract'
+
+export type {
+  PingRequest,
+  PingResponse,
+} from './system/ping.contract'
